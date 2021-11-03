a52c7b66fcbb56d01473aa3da7a62427c24aebb5
Bombastion <tyler.tift@gmail.com>
2021-11-02 21:52:16 -0600

    Basic logging, populate landing page

--- db/model/model_handler.py
+++ db/model/model_handler.py
@@ -1,6 +1,15 @@
-from typing import Any
+from dataclasses import dataclass
+import logging
+from typing import Any, List
+
+from sqlalchemy.sql.expression import text
 
 from db.model import Client, Item, ItemCollection, ItemType
+
+class ItemTypeQuery:
+    def __init__(self, ids: List[str]=None, text_search: str = None):
+        self.ids = ids
+        self.text_search = text_search
 
 class ModelHandler:
     def __init__(self, session_factory):
@@ -17,3 +26,13 @@ class ModelHandler:
         session = self.session_factory()
         session.delete(object)
         session.commit()
+
+    def list_item_types(self, query: ItemTypeQuery) -> List[ItemType]:
+        session = self.session_factory()
+        criteria = []
+        if query.ids:
+            criteria.append(ItemType.item_type_id.in_(query.ids))
+        if query.text_search:
+            criteria.append(ItemType.name.match(query.text_search))
+
+        return session.query(ItemType).filter(*session).order_by(ItemType.item_type_id).all()
--- service/flask_service.py
+++ service/flask_service.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 import logging
+from db.model import item_type
 
-import db.model
+from db.model.model_handler import ItemTypeQuery, ModelHandler
+from db.model.sqlalchemy_base import SessionFactory
 
 app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-   return render_template('index.html', name="Fish")
+   handler = ModelHandler(SessionFactory)
+   query = ItemTypeQuery()
+   item_types = handler.list_item_types(query)
+   return render_template('index.html', item_types=item_types)
 
 
 if __name__ == '__main__':
+   formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+   logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding='utf-8', level=logging.DEBUG, )
    app.run(host="0.0.0.0")
