70853c0af7562964803a6886268b9cc1b1b33ed5
Tyler Tift <tyler.tift@gmail.com>
2021-12-09 14:26:25 -0700

    Adds user auth (#25)
    * Adding some templates based on tutorial
    
    * Moving to ID based jQuery
    
    * Scaffolding the login/signup pages
    
    * Finalizing login

--- db/model/client.py
+++ db/model/client.py
@@ -1,15 +1,21 @@
 from typing import Dict
 
+from flask_login import UserMixin
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import UUID
 
 from db.model.sqlalchemy_base import Base
 
-class Client(Base):
+class Client(UserMixin, Base):
     __tablename__ = "client"
     client_id = sa.Column(UUID, primary_key=True, server_default=sa.text("uuid_generate_v4()"))
     name = sa.Column(sa.Text, nullable=False)
     email = sa.Column(sa.Text, nullable=False)
+    password = sa.Column(sa.Text, nullable=False)
+
+    # Required by flask_login.login_user 
+    def get_id(self):
+        return self.client_id
 
     def __repr__(self) -> str:
         return f"Client(client_id={self.client_id}, name={self.name}, email={self.email})"
